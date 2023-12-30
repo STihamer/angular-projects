26783388e468fef592bd90838f21bd4bc12d51d8
STihamer <tihamer.sebok@gmail.com>
2023-12-30 16:24:45 +0200

    session 4

--- TemplateReference/src/app/app.component.ts
+++ TemplateReference/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,33 @@ import { CustomerListComponent } from './customer-list/customer-list.component';
 export class AppComponent {
   title = 'TemplateReference';
 
+  @ViewChild('dobInput') dateOfBirth!: ElementRef;
+  @ViewChild('ageInput') age!: ElementRef;
   logCustomer(component: CustomerListComponent) {
     console.log(component);
+  }
+
+  calculateAge() {
+    let birthYear = parseInt(
+      this.dateOfBirth.nativeElement.value.substring(0, 4)
+    );
+
+    // let birthYear = new Date(this.dateOfBirth.nativeElement.value).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.age.nativeElement.value = age;
+  }
+
+  @ViewChild(CustomerListComponent, { static: true })
+  customerListComponent!: CustomerListComponent;
+
+  @ViewChild(DemoComponent)
+  demoComponent!: DemoComponent;
+
+  constructor() {
+    setTimeout(() => {
+      console.log(this.customerListComponent.customers), 1;
+      console.log(this.demoComponent), 1;
+    });
   }
 }
